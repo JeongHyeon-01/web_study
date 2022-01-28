bd8673fb39d02cf9504defe32ae7722494b8e6ec
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-28 20:51:42 +0900

    first commit

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def blog(request):
+    postlist = Post.objects.all()
+    return render(request, 'main/blog.html', {'postlist': postlist})
+
+def posting(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'main/posting.html', {'post': post})
